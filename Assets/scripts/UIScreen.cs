91c7fd9259ac2d777183e8f1d2343552881133c7
강언덕 <64720058+123456789qwaszx@users.noreply.github.com>
2025-12-15 08:59:21 +0900

    chore: add stubbed out UIScreen + UIBinder

--- Assets/scripts/UIScreen.cs
+++ Assets/scripts/UIScreen.cs
@@ -1,6 +1,33 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class UIScreen : MonoBehaviour
 {
-    
+    private Dictionary<string, RectTransform> _slots;
+    public RectTransform GetSlot(string name)
+    {
+        return _slots[name];
+    }
+
+    public void Build(UIBinder binder)
+    {
+        _slots = binder.BuildSlots(transform);
+    }
+
+    public void Open()  => gameObject.SetActive(true);
+    public void Close() => Destroy(gameObject);
+}
+
+public class UIBinder
+{
+    private string[] SlotNames = { "Header", "Body", "Footer" };
+
+    public Dictionary<string, RectTransform> BuildSlots(Transform root)
+    {
+        Dictionary<string, RectTransform> dict = new();
+        foreach (String name in SlotNames)
+            dict[name] = root.Find(name) as RectTransform;
+        return dict;
+    }
 }
