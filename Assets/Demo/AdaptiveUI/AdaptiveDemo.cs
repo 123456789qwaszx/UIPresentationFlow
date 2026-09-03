315079e72319348d5677d35bcfb10a30794a6f16
강언덕 <64720058+123456789qwaszx@users.noreply.github.com>
2026-09-03 22:58:59 +0900

    refactor: 레거시 스크립트 제거(UIScreen, WidgetHandle)

--- Assets/Demo/AdaptiveUI/AdaptiveDemo.cs
+++ Assets/Demo/AdaptiveUI/AdaptiveDemo.cs
@@ -1,6 +1,13 @@
-public sealed class AdaptiveDemo : UIBase<AdaptiveDemo.Refs>
+public sealed class AdaptiveDemoUIRoot : UIBase<AdaptiveDemoUIRoot.Refs>
 {
     public enum Refs
     {
+        Background,
+        Header,
+        PrimaryContent,
+        SideInfo,
+        BottomControls,
+        LeftAction,
+        RightAction,
     }
 }
